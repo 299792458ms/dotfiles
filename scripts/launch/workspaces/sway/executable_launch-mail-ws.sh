66c2ebe308081b30c95871f40269dbab7ee2d44a
299792458ms <alecsargent@ik.me>
2024-11-03 01:08:25 -0300

    update 014
    - change mail to aerc
    - some other stuff

--- scripts/launch/workspaces/sway/executable_launch-mail-ws.sh
+++ scripts/launch/workspaces/sway/executable_launch-mail-ws.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# if swaymsg -t get_tree | rg mail80085; then
-#   swaymsg workspace κ
-#   else
-#   swaymsg workspace κ
-#   footclient -a mail80085 -e aerc
-# fi
-
-if swaymsg -t get_tree | rg geary; then
+if swaymsg -t get_tree | rg mail80085; then
   swaymsg workspace κ
   else
   swaymsg workspace κ
-  geary
+  footclient -a mail80085 -e aerc
 fi
+
+# if swaymsg -t get_tree | rg geary; then
+#   swaymsg workspace κ
+#   else
+#   swaymsg workspace κ
+#   geary
+# fi
